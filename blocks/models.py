db38a8ea457e8e6e5b5d43797993fecd90638a6a
pcfromdr <ltimofej998@gmail.com>
2026-07-24 17:48:00 +0300

    registration

--- blocks/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class block(models.Model):
-    title = models.CharField(max_length=60)
-    description = models.TextField(max_length=1000)
-
-    def __str__(self):
-        return self.title
-
